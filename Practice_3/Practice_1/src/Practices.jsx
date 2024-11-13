4eaba1f0efc32812cf2e1105e2428df50c5bc126
Umair Ahmed <umairnoonar98@gmail.com>
2024-11-13 22:12:36 +0500

    More on Inteview Q/A

--- Practice_3/Practice_1/src/Practices.jsx
+++ Practice_3/Practice_1/src/Practices.jsx
@@ -1,9 +1,10 @@
 export const Practices=()=>{
     const students=[]
     return<div>
+        <p>{students.length && "No students Found"}</p>
         {/* <p>{students.length===0 && "No students Found"}</p> */}
         {/* <p>{!students.length && "No students Found"}</p> */}
-        <p>{!Boolean(students.length) && "No students Found"}</p>
+        {/* <p>{!Boolean(students.length) && "No students Found"}</p> */}
         <p>Number of students:{students.length}</p>
     </div>
 }
