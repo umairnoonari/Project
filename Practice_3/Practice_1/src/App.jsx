c826ac4d6e4f8d414a7136ea2fec34a4bd7ecc7c
Umair Ahmed <umairnoonar98@gmail.com>
2024-11-13 20:37:54 +0500

    add More things

--- Practice_3/Practice_1/src/App.jsx
+++ Practice_3/Practice_1/src/App.jsx
@@ -16,20 +16,50 @@ const App = () => {
   );
 };
 const NetflixSeries=()=>{
+  const rating=8.2;
+  const name="Queen of Tears"
+  const summary="Queen of Tears is a K-drama that tells the story of a couple who have become distant due to circumstances. An ill-fated twist in their life brings them together and gives them the courage to fight their fate1. The show offers a fresh and transformative take on K-romance, with predictable yet startlingly human elements"
+  const age=17
+  // let canWatch="Not Available"
+  // if(age>=18) canWatch="Watch Now"
+  const canWatch=()=>{
+    if(age>=18) return "Watch Now"
+    return "Not Available"
+  }
+  const Genre=()=>{
+    const genre="RomCom"
+    return genre
+  }
+  // if(age<18)
+  // {
+  //     return<Fragment>
+  //     <div>
+  //       <img src= "qot.jpg" alt="qot.jpg" width="40%" height="40%"/>
+  //     </div>
+  //     <h2>Name: {name}</h2>
+  //     <h3>Rating:{5+3.3}</h3>
+  //     <p>
+  //       Summary:{summary}
+  //     </p>
+  //     <p>Genre: {Genre()}</p>
+  //     <button>Not Available</button>
+  //     </Fragment>
+  // }
   return(
   <Fragment>
         <div>
           <img src= "qot.jpg" alt="qot.jpg" width="40%" height="40%"/>
         </div>
-        <h2>Name: Queen of Tears</h2>
-        <h3>Rating:8.2</h3>
+        <h2>Name: {name}</h2>
+        <h3>Rating:{5+3.3}</h3>
         <p>
-          Summary:Queen of Tears is a K-drama that tells the story of a couple who
-          have become distant due to circumstances. An ill-fated twist in their
-          life brings them together and gives them the courage to fight their
-          fate1. The show offers a fresh and transformative take on K-romance,
-          with predictable yet startlingly human elements
+          Summary:{summary}
         </p>
+        <p>Genre: {Genre()}</p>
+        {/* <button>{age>=18?"Watch Now":"Not Available"}</button> */}
+        {/* <button>{canWatch}</button> */}
+        <button>{canWatch()}</button>
+
   </Fragment>
   )
 }
